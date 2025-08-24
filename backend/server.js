1037f686f60882482814ce60bbf7376c8ba4171f
leontoys <svliyon@gmail.com>
2025-08-25 06:56:05 +1000

    created basic api route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express"
 
 const app = express()
 
+app.get('/api/notes', (req, res) => {
+    res.status(200).send('here are your notes')
+})
+
 app.listen(5001, () => {
     console.log('app listening on PORT 5001')
 })
