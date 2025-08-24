5d36627aed231906d5ac4cada69e8b551d8420aa
leontoys <svliyon@gmail.com>
2025-08-25 07:09:35 +1000

    created all CRUD routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,18 @@ app.get('/api/notes', (req, res) => {
     res.status(200).send('here are your notes')
 })
 
+app.post('/api/notes', () => {
+    res.status(201).json({message:'note created successfully'})
+})
+
+app.put('/api/notes/:id', (req, res) => {
+    res.status(200).json({message:'note updated successfully'})
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    res.status(200).json({message:'note deleted successfully'})
+})
+
 app.listen(5001, () => {
     console.log('app listening on PORT 5001')
 })
