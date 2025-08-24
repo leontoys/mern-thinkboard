3dced3579d45148b359a4ba6a383b9264ff0120a
leontoys <svliyon@gmail.com>
2025-08-25 07:23:08 +1000

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,8 @@
 import express from "express"
+import notesRoutes from "./routes/notesRoutes.js"
 
 const app = express()
-
-app.get('/api/notes', (req, res) => {
-    res.status(200).send('here are your notes')
-})
-
-app.post('/api/notes', () => {
-    res.status(201).json({message:'note created successfully'})
-})
-
-app.put('/api/notes/:id', (req, res) => {
-    res.status(200).json({message:'note updated successfully'})
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-    res.status(200).json({message:'note deleted successfully'})
-})
+app.use('/api/notes',notesRoutes)
 
 app.listen(5001, () => {
     console.log('app listening on PORT 5001')
