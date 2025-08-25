6661c2fdd03433f3892f9b7f7b7e9eb5e2390c0f
leontoys <svliyon@gmail.com>
2025-08-26 08:17:34 +1000

    created front end and routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from "react-router";
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/create' element={<CreatePage/>}></Route>
+        <Route path='/note/:id' element={<NoteDetailPage/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
