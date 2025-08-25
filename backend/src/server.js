fb9f887e9e0b0908d7b83b277374c25f0c36ac15
leontoys <svliyon@gmail.com>
2025-08-26 07:45:20 +1000

    ratelimiting done

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,18 @@ import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
+import rateLimiter from "./middleware/rateLimiter.js"
 
 dotenv.config()
 const app = express()
 app.use(express.json())
+app.use(rateLimiter)
 app.use('/api/notes', notesRoutes)
-connectDB()
 
+connectDB().then(() => {
 app.listen(process.env.PORT, () => {
-    console.log(`app listening on http://localhost:${process.env.PORT}/api/notes`);
+  console.log(
+    `app listening on http://localhost:${process.env.PORT}/api/notes`
+  );
+});  
 })
