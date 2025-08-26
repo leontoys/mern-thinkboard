665a991b97cf39190bfc8d8f3c87c92a877a239d
leontoys <svliyon@gmail.com>
2025-08-27 07:13:22 +1000

    installed axios and cors and got data from the backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,11 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js"
+import cors from "cors"
 
 dotenv.config()
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(rateLimiter)
 app.use('/api/notes', notesRoutes)
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from "../components/Navbar";
 import RateLimitedUI from '../components/RateLimitedUI';
+import axios from "axios"
 
 
 const HomePage = () => {
-  const[isRateLimited,setIsRateLimited] = useState(false)
+  const [isRateLimited, setIsRateLimited] = useState(false)
+  const [notes, setNotes] = useState([])
+  const[loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const response = await axios.get("http://localhost:5001/api/notes")
+      console.log(response.data)
+    }
+    fetchNotes()
+  },[])
 
   return (
     <div className='min-h-screen'>
