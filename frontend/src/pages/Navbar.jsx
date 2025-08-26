ad4542de20a0633ec33dffe3e2d2f771c906f307
leontoys <svliyon@gmail.com>
2025-08-27 06:29:37 +1000

    created new note button on the rightside with lucide react

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router'
+import { PlusIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
       <header className='bg-base-300 border-b border-base-content'>
           <div className='mx-auto mx-w-6xl p-4'>
               <div className='flex items-center justify-between'>
-                  <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>ThinkBoard</h1>
+          <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>ThinkBoard</h1>
+          <div className='flex items-center gap-4'>
+            <Link to={"/create"} className='btn btn-primary'>
+              <PlusIcon className="size-5"></PlusIcon>
+              <span>New Note</span>
+            </Link>
+          </div>
               </div>
           </div>
     </header>
