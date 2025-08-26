25be11214d5ebdd6a2f3eaf2c584037df57678dd
leontoys <svliyon@gmail.com>
2025-08-27 06:22:12 +1000

    navbar created

---
+++ frontend/src/pages/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <header className='bg-base-300 border-b border-base-content'>
+          <div className='mx-auto mx-w-6xl p-4'>
+              <div className='flex items-center justify-between'>
+                  <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>ThinkBoard</h1>
+              </div>
+          </div>
+    </header>
+  )
+}
+
+export default Navbar
