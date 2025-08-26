5ce9c92e108370547d3557b275c313f5d0405118
leontoys <svliyon@gmail.com>
2025-08-27 07:31:00 +1000

    showing notes in the frontend basic form

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -35,7 +35,16 @@ const HomePage = () => {
   return (
     <div className='min-h-screen'>
       <Navbar />
-      {isRateLimited && <RateLimitedUI/>}
+      {isRateLimited && <RateLimitedUI />}
+      {notes.length > 0 && !isRateLimited && (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+          {notes.map((note) => (
+            <div>
+              {note.title}|{note.content}
+              </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
