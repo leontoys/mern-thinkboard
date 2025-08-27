fe45926a136939ac53e4e0e69764aebe0c20d72a
leontoys <svliyon@gmail.com>
2025-08-28 07:58:53 +1000

    create page ui in progress

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { ArrowLeftIcon } from 'lucide-react'
+import { Link } from 'react-router'
 
 const CreatePage = () => {
   return (
-    <div>CreatePage</div>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container mx-auto px-4 py-8'>
+        <div className='max-w-2xl mx-auto'>
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeftIcon className="size-5" />
+                          Back to Notes
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
