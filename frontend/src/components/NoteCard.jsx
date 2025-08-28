9a8881ca55db97300a7e9fdb5c394da360fc6d99
leontoys <svliyon@gmail.com>
2025-08-29 06:56:09 +1000

    note deleted but page not refreshing

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -2,8 +2,24 @@ import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router'
 import { formatDate } from '../lib/utils'
+import toast from 'react-hot-toast'
+import api from '../lib/axios'
 
-const NoteCard = ({note}) => {
+const NoteCard = ({ note }) => {
+    const handleDelete = async (e, id) => {
+        e.preventDefault() //to stop navigating to detail page on click of card
+        if (!window.confirm("Are you sure you want to delete?")) return
+
+        try {
+            await api.delete(`/notes/${id}`)
+            toast.success('Note deleted successfully')
+        
+    } catch (error) {
+        console.error(error)
+        toast.error('Error while deleting note')
+    }    
+  }
+
   return (
       <Link to={`note/${note._id}`}
           className='card bg-base-100 hover:shadow-lg transition-all duration-200
@@ -17,7 +33,7 @@ const NoteCard = ({note}) => {
                   </span>
                   <div className='flex items-center gap-1'>
                       <PenSquareIcon className='size-4'></PenSquareIcon>
-                      <button className='btn btn-ghost btn-xs text-error'>
+                      <button className='btn btn-ghost btn-xs text-error' onClick={(e)=>handleDelete(e,note._id)}>
                           <Trash2Icon className='size-4'></Trash2Icon>
                       </button>
                   </div>
