850468f4d23b2a2f10453e782d2746f0c4deffe9
leontoys <svliyon@gmail.com>
2025-08-30 07:14:48 +1000

    works in production mode

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,27 @@ import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js"
 import cors from "cors"
+import path from "path"
 
 dotenv.config()
 const app = express()
-app.use(cors())
+const __dirname = path.resolve()
+console.log(__dirname)
+if (process.env.NODE_ENV!=="production") {
+ app.use(cors()); 
+}
 app.use(express.json())
 app.use(rateLimiter)
 app.use('/api/notes', notesRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/../frontend/dist")));
+  app.get(/(.*)/, (req, res) => { //NOTE- app.get("*") doesn't work in node5 and above
+    console.log("hello-world");
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 connectDB().then(() => {
 app.listen(process.env.PORT, () => {
